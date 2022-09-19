21e92ea74956b508bc0b38b112eece930610bbfe
Cologler <skyoflw@gmail.com>
2022-09-20 01:19:14 +0800

    use stdout logging

--- rssfetcher.py
+++ rssfetcher.py
@@ -268,7 +268,6 @@ class RssFetcherWorker:
 
 def configure_logger():
     logging_options = dict(
-        filename='rssfetcher.log',
         format='%(asctime)s [%(levelname)s] - %(name)s: %(message)s',
         datefmt='%m/%d/%Y %I:%M:%S %p',
         level=logging.INFO
