5a6a9df3910da6fbf5c9ff9cf9e82c7295b5908f
Cologler <skyoflw@gmail.com>
2022-09-20 14:13:49 +0800

    move sqlite store to stores

--- rssfetcher/__init__.py
+++ rssfetcher/__init__.py
@@ -9,7 +9,6 @@
 
 from typing import *
 import logging
-import sqlite3
 from urllib.parse import urlparse
 import xml.etree.ElementTree as et
 import os
@@ -28,6 +27,8 @@ import schedule
 import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseSettings
+
+from .stores import open_store
 
 @cache
 def get_logger():
@@ -96,71 +97,13 @@ def fetch_feed(feed_id: str, feed_section: dict):
                 logger.info('total found %s items',len(items))
     return items
 
-
-class RssStore:
-    COLUMN_NAMES = ('feed_id', 'rss_id', 'title', 'raw')
-
-
-class SqliteRssStore(RssStore):
-    TABLE_NAME = 'rss'
-
-    def __init__(self, conn_str: str) -> None:
-        self._conn = sqlite3.connect(conn_str)
-        self._cur = None
-
-    def __enter__(self):
-        self._cur = self._conn.cursor()
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self._cur and self._cur.close()
-        self._conn and self._conn.close()
-
-    def init_store(self):
-        DEF_COL = ', '.join([
-            self.COLUMN_NAMES[0] + ' TEXT NOT NULL',
-            self.COLUMN_NAMES[1] + ' TEXT NOT NULL',
-            self.COLUMN_NAMES[2] + ' TEXT',
-            self.COLUMN_NAMES[3] + ' TEXT',
-            'PRIMARY KEY ({}, {})'.format(self.COLUMN_NAMES[0], self.COLUMN_NAMES[1]),
-        ])
-        SQL_CREATE = 'CREATE TABLE IF NOT EXISTS {} ({});'.format(self.TABLE_NAME, DEF_COL)
-        self._cur.execute(SQL_CREATE)
-
-    def get_count(self) -> int:
-        SQL_COUNT = 'SELECT COUNT({}) FROM {}'.format(self.COLUMN_NAMES[0], self.TABLE_NAME)
-        return self._cur.execute(SQL_COUNT).fetchone()[0]
-
-    def upsert(self, items):
-        SQL_INSERT = 'INSERT OR IGNORE INTO {} VALUES ({});'.format(self.TABLE_NAME, ",".join("?" for _ in self.COLUMN_NAMES))
-        self._cur.executemany(SQL_INSERT, items)
-
-    def remove_old_items(self, kept_count: int):
-        assert isinstance(kept_count, int) and kept_count > 0 # hard limit
-
-        max_rowid = self._cur.execute('SELECT MAX(ROWID) FROM {}'.format(self.TABLE_NAME)).fetchone()[0]
-        return self._cur.execute('DELETE FROM {} WHERE ROWID <= {}'.format(self.TABLE_NAME, max_rowid - kept_count)).rowcount
-
-    def commit(self):
-        self._conn.commit()
-
-    def read_items(self, start_rowid: int, limit: int):
-        sql = 'SELECT ROWID, * FROM {} WHERE ROWID > {} ORDER BY ROWID LIMIT {}'.format(
-            self.TABLE_NAME, start_rowid, limit
-        )
-
-        self._cur.row_factory = sqlite3.Row
-        reader = self._cur.execute(sql)
-        items = reader.fetchall()
-        return items
-
 def _conf_iter_feeds(conf_data: dict):
     default = conf_data.get('default', {})
     for feed_id, feed_section in conf_data.get('feeds', {}).items():
         yield feed_id, ChainMap(feed_section, default)
 
 def _conf_open_store(conf_data: dict):
-    return SqliteRssStore(conf_data.get('database', 'rss.sqlite3'))
+    return open_store(conf_data.get('database', 'rss.sqlite3'))
 
 def _fetch_feeds(conf_data: dict, feeds: list):
     options = conf_data.get('options', {})
