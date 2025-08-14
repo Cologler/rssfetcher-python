8dd13bca87d584e3674d7e278b95a9f839c7c3ab
Cologler <skyoflw@gmail.com>
2025-08-14 13:36:00 +0800

    chore: update some typing

--- rssfetcher/server.py
+++ rssfetcher/server.py
@@ -5,24 +5,26 @@
 #
 # ----------
 
+from collections.abc import AsyncIterator, Iterator
 from contextlib import asynccontextmanager
-from typing import Annotated
+from typing import Annotated, cast
 
 from fastapi import APIRouter, Depends, FastAPI, HTTPException, Request, Response, status
 from fastapi.security import APIKeyHeader, APIKeyQuery
 
-from .cfg import Config
+from .cfg import Config, ConfigHelper
 from .core import RssFetcherWorker, configure_logger, load_config_helper
 from .settings import SettingsDeps
 from .stores import SqliteRssStore
 
 
-def _get_config_from_request(request: Request):
-    return request.app.state.config_helper.get_config()
+def _get_config_from_request(request: Request) -> Config:
+    config_helper = cast(ConfigHelper, request.app.state.config_helper)
+    return config_helper.get_config()
 
 ConfigDeps = Annotated[Config, Depends(_get_config_from_request)]
 
-def _open_store(config: ConfigDeps):
+def _open_store(config: ConfigDeps) -> Iterator[SqliteRssStore]:
     with config.open_store() as store:
         yield store
 
@@ -33,8 +35,7 @@ def _verify_api_key(
     settings: SettingsDeps,
     header_api_key: Annotated[str | None, Depends(APIKeyHeader(name="x-key", auto_error=False))],
     query_api_key: Annotated[str | None, Depends(APIKeyQuery(name="api_key", auto_error=False))],
-):
-    print(header_api_key, query_api_key)
+) -> None:
     if secret_key := settings.secret_key:
         if not header_api_key and not query_api_key:
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authenticated")
@@ -50,7 +51,7 @@ router = APIRouter()
 async def get_items(
     store: StoreDeps,
     start_rowid: int = 0, limit: int | None = None,
-):
+) -> dict:
     limit_max = 1000
     limit = min(max(limit, 1), limit_max) if isinstance(
         limit, int) else limit_max
@@ -66,7 +67,7 @@ async def get_items(
 @router.get("/status", dependencies=[Depends(_verify_api_key)])
 async def get_status(
     store: StoreDeps,
-):
+) -> dict:
     return {
         'count': store.get_count(),
         'min_id': store.get_min_id(),
@@ -76,12 +77,12 @@ async def get_status(
 
 @router.head('/ping')
 @router.get('/ping')
-def ping():
+def ping() -> Response:
     return Response(status_code=200)
 
 
 @asynccontextmanager
-async def lifespan(app: FastAPI):
+async def lifespan(app: FastAPI) -> AsyncIterator[None]:
     configure_logger()
 
     config_helper = load_config_helper()
